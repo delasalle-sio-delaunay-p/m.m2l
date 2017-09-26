f827707d70e3ee515513fe926fab4e14bd545b1a
delaunay.p <delaunay.p@I12-03.sio.local>
2017-09-26 16:42:21 +0200

    Ajout de la page test.php

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+<html>
+<header>
+<title>Salut à tous</title>
+</header>
+
+
+<body>
+<H1>Veille juridique</H1>
+<?php // <img src="http://www.tfvb.fr/images/Lala.png"> ?>
+
+</body>
+
+</html>
+
+<?php
+?>

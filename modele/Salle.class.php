efb4c53a7a405d165409845fb5d1fb7740d88a5a
delasalle-sio-delaunay-p <delasalle.sio.delaunay.pierre@gmail.com>
2017-10-03 14:21:02 +0200

    Mise à jour classe Salle par Pierre

--- modele/Salle.class.php
+++ modele/Salle.class.php
@@ -11,30 +11,53 @@ class Salle {
     // ---------------------------------- Membres privés de la classe ---------------------------------------
     // ------------------------------------------------------------------------------------------------------
     
-    
+    private $id;        // identifiant de la salle
+    private $room_name; // nom de la salle
+    private $capacity;  // capacité de la salle
+    private $area_name; // nom de la zone
     
     
     // ------------------------------------------------------------------------------------------------------
     // ----------------------------------------- Constructeur -----------------------------------------------
     // ------------------------------------------------------------------------------------------------------
     
-    
+    public function Salle($unId, $unRoomName, $uneCapacity, $unAreaName) {
+        $this->id = $unId;
+        $this->room_name = $unRoomName;
+        $this->capacity = $uneCapacity;
+        $this->area_name = $unAreaName;
+    }
     
     
     // ------------------------------------------------------------------------------------------------------
     // ---------------------------------------- Getters et Setters ------------------------------------------
     // ------------------------------------------------------------------------------------------------------
     
-    private $id;        // identifiant de la salle
-    private $room_name; // nom de la salle
-    private $capacity;  // capacité de la salle
-    private $area_name; // nom de la zone
+    public function getId()	{return $this->id;}
+    public function setId($unId) {$this->id = $unId;}
+    
+    public function getRoom_name()	{return $this->room_name;}
+    public function setRoom_name($unRoomName) {$this->room_name = $unRoomName;}
+    
+    public function getCapacity()	{return $this->capacity;}
+    public function setCapacity($uneCapacity) {$this->capacity = $uneCapacity;}
+    
+    public function getAreaName()	{return $this->area_name;}
+    public function setAreaName($unAreaName) {$this->area_name = $unAreaName;}
     
     // ------------------------------------------------------------------------------------------------------
     // ---------------------------------------- Méthodes d'instances ----------------------------------------
     // ------------------------------------------------------------------------------------------------------
-    
-    
+
+    public function toString() {
+        $msg = "Salle : <br>";
+        $msg .= "id : " . $this->id . "<br>";
+        $msg .= "room_name : " . $this->room_name . "<br>";
+        $msg .= "capacity : " . $this->capacity . "<br>";
+        $msg .= "area_name : " . $this->area_name . "<br>";
+        return $msg;
+    }
+
 } // fin de la classe Salle
 
 

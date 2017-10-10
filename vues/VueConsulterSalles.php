56a37d7fbcce9db969c8ca1ad2bc33eb141bfc62
delasalle-sio-delaunay-p <delasalle.sio.delaunay.pierre@gmail.com>
2017-10-10 16:17:29 +0200

    Vue/Contrôleur consulter Salles - Pierre - 10/10/2017 16h17

--- vues/VueConsulterSalles.php
+++ vues/VueConsulterSalles.php
@@ -19,29 +19,22 @@
 				<a href="index.php?action=Menu" data-transition="<?php echo $transition; ?>">Retour menu</a>
 			</div>
 			<div data-role="content">
-				<h4 style="text-align: center; margin-top: 0px; margin-bottom: 0px;">Consulter mes réservations</h4>
-				<p style="text-align: center;"><?php echo $message; ?></p>
+				<h4 style="text-align: center; margin-top: 0px; margin-bottom: 0px;"><?php echo $message; ?></h4>
+				<p style="text-align: center;"><?php //echo $message; ?></p>
 				<ul data-role="listview" style="margin-top: 5px;">
 				<?php
 				// Avec jQuery Mobile, les réservations sont affichées à l'aide d'une liste <ul>
 				// chaque réservation est affichée à l'aide d'un élément de liste <li>
 				// chaque élément de liste <li> peut contenir des titres et des paragraphes
 
-				foreach ($lesReservations as $uneReservation)
+				foreach ($lesSalles as $uneSalle)
 				{ ?>
 					<li><a href="#">
-					<h5>Réserv. n° <?php echo $uneReservation->getId(); ?></h5>
-					<p>Passée le <?php echo Outils::convertirEnDateFR(substr($uneReservation->getTimestamp(), 0, 10)); ?></p>
-					<p>Début : <?php echo date('d/m/Y H:i:s', $uneReservation->getStart_time()); ?></p>
-					<p>Fin : <?php echo date('d/m/Y H:i:s', $uneReservation->getEnd_time()); ?></p>
-					<p>Salle : <?php echo $uneReservation->getRoom_name(); ?></p>
-					<p>Etat : <?php if ($uneReservation->getStatus() == 0) 
-										echo 'confirmée';
-					 				else 
-					 					echo 'provisoire';?></p>
-					<?php if ($uneReservation->getStatus() == 0) 
+					<h5>Salle : <?php echo $uneSalle->getRoom_name(); ?></h5>
+					<p>Domaine : <?php echo $uneSalle->getAreaName(); ?></p>
+					<?php if ($uneSalle->getCapacity() > 0) 
 							// la classe "ui-li-aside" de JQuery Mobile permet de positionner un élément à droite
-							echo '<h5 class="ui-li-aside">Digicode ' . $uneReservation->getDigicode() . '</h5>';?>
+							echo '<h5 class="ui-li-aside"> ' . $uneSalle->getCapacity() . ' places </h5>';?>
 					</a></li>
 				<?php
 				} ?>
